572940c479f0f6260389339c0042fcfd761a4dfb
zjb <fillixzhang@163.com>
2019-12-17 16:44:58 +0800

    zuul三种路由配置

---
+++ zuulservice/src/main/java/com/zjb/ZuulserviceApplication.java
@@ -0,0 +1,15 @@
+package com.zjb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy //使服务成为一个Zuul服务器
+public class ZuulserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulserviceApplication.class, args);
+    }
+
+}
